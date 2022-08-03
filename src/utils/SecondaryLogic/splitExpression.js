2e57210bb92bff2990fec9bc98d94f0b1cd931f7
Fritterus <62291430+Fritterus@users.noreply.github.com>
2022-08-03 03:29:13 +0300

    Create splitExpression.js

---
+++ src/utils/SecondaryLogic/splitExpression.js
@@ -0,0 +1,36 @@
+import {
+  OPERATIONS, PLUS_MINUS, OPERATIONS_NO_BRACKETS,
+} from '@/constants/index';
+
+export const getSplittedExpression = (expression) => {
+  let counter = 0;
+
+  const splitted = expression.replace(/[+\-*/%()]/g, (match) => ` ${match} `)
+    .split(' ')
+    .filter((x) => x !== ' ' && x !== '')
+    .map((x) => (OPERATIONS.includes(x) ? x : +x))
+    .reduce((acc, curr) => {
+      if (!isNaN(curr)) {
+        if (PLUS_MINUS.includes(acc[counter - 1])
+              && (acc[counter - 2] === '('
+              || OPERATIONS_NO_BRACKETS.includes(acc[counter - 2])
+              || acc[counter - 2] === undefined)) {
+          acc[counter - 1] += curr;
+          return acc;
+        }
+        if (acc[counter - 1] === ')') {
+          throw new Error('');
+        }
+        counter++;
+        return [...acc, curr];
+      }
+      if (curr === '(' && !isNaN(acc[counter - 1])) {
+        counter += 2;
+        return [...acc, '*', curr];
+      }
+      counter++;
+      return [...acc, curr];
+    }, []);
+
+  return splitted;
+};
