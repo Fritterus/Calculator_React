d68ce7a4e4fba7243e3ad57440718b41d2e6c782
Fritterus <62291430+Fritterus@users.noreply.github.com>
2022-08-03 03:28:53 +0300

    rewrite and move keypad logic

--- src/containers/Keypad/Keypad.jsx
+++ src/containers/Keypad/Keypad.jsx
@@ -2,101 +2,16 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import * as Styled from './components';
 import { BUTTON_VALUES } from '@/constants/index';
-import { writeDownExpr, allClear, clearLast } from '@/reducers/actionCreators/expression';
-import { writeHistory } from '@/reducers/actionCreators/history';
-import { getAnswer } from '@/utils/calculator';
-import { PLUS_MINUS } from '@/constants/operations';
+import { useWriteDown, useClearLast, useCalculate } from '@/hooks/index';
+import { allClear } from '@/reducers/actionCreators/expression';
+import { getLongBtnStyles } from './buttonStyles';
 
-const operations = [
-  '/', '*', '%',
-];
-
-const getStylesLongBtn = (value) => {
-  let styles = {};
-
-  if (value === '=') {
-    styles = Styled.longEqualBtn;
-  }
-  if (value === 0) {
-    styles = Styled.longZeroBtn;
-  }
-  if (value === 'C') {
-    styles = Styled.longAllClearBtn;
-  }
-  if (value === 'CE') {
-    styles = Styled.longClearBtn;
-  }
-
-  return styles;
-};
-
-const Keypad = () => {
+export const Keypad = () => {
   const expression = useSelector((state) => state.expression);
   const dispatch = useDispatch();
-
-  const writeDownHandler = (value) => {
-    const expr = expression.currentExpression;
-
-    if (expr === 'Error'
-        || expr === 'Infinity') {
-      dispatch(clearLast(''));
-    }
-    if (
-      expr === '0'
-            && !operations.includes(value)
-    ) {
-      dispatch(clearLast(''));
-    }
-    if (
-      expr === '0'
-            && operations.includes(value)
-    ) {
-      return;
-    }
-    if (value === '+' && expr[expr.length - 1] === '-'
-    || value === '-' && expr[expr.length - 1] === '+') {
-      dispatch(clearLast(expr.slice(0, -1)));
-      dispatch(writeDownExpr(value));
-      return;
-    }
-    if (PLUS_MINUS.includes(value) && PLUS_MINUS.includes(expr[expr.length - 1])) {
-      return;
-    }
-    if (
-      operations.includes(value) && operations.includes(expr.slice(-1))
-            || operations.includes(value) && expr === ''
-            || value === '.' && expr.slice(-1) === '.'
-    ) {
-      return;
-    }
-
-    dispatch(writeDownExpr(value));
-  };
-
-  const clearLastHandler = (expression) => {
-    if (expression.length === 1) {
-      dispatch(allClear());
-    } else {
-      dispatch(clearLast(expression.slice(0, -1)));
-    }
-  };
-
-  const equalsClickHandler = () => {
-    const answer = getAnswer(expression.currentExpression);
-    const expr = expression.currentExpression;
-
-    if (answer === expr) {
-      return;
-    }
-    if (answer === 'Error') {
-      dispatch(clearLast(answer));
-      return;
-    }
-    if (answer !== 'Error') {
-      dispatch(clearLast(answer));
-      dispatch(writeHistory(expression.currentExpression));
-    }
-  };
+  const writeDown = useWriteDown(dispatch, expression);
+  const clearLastSymbol = useClearLast(dispatch);
+  const calculate = useCalculate(dispatch, expression);
 
   const clickHandler = (e) => {
     switch (e.target.textContent) {
@@ -105,16 +20,15 @@ const Keypad = () => {
         break;
       }
       case 'CE': {
-        clearLastHandler(expression.currentExpression);
+        clearLastSymbol(expression.currentExpression);
         break;
       }
       case '=': {
-        equalsClickHandler();
+        calculate();
         break;
       }
       default: {
-        writeDownHandler(e.target.textContent);
-        console.log(expression.currentExpression);
+        writeDown(e.target.textContent);
       }
     }
   };
@@ -123,20 +37,18 @@ const Keypad = () => {
         <Styled.Keypad>
             {BUTTON_VALUES.map(
               (value, id) => <Styled.Button
-                        long={
-                            getStylesLongBtn(value)
-                        }
+                              long={
+                                getLongBtnStyles(value)
+                              }
 
-                        key={id}
+                              key={id}
 
-                        onClick={clickHandler}
+                              onClick={clickHandler}
 
-                    >
+                              >
                         {value}
                     </Styled.Button>,
             )}
         </Styled.Keypad>
   );
 };
-
-export default Keypad;
