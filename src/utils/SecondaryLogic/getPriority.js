c14148acf9a29a404d4fd9d903c4b3dfb6c000fd
Fritterus <62291430+Fritterus@users.noreply.github.com>
2022-08-03 03:29:10 +0300

    Create getPriority.js

---
+++ src/utils/SecondaryLogic/getPriority.js
@@ -0,0 +1,17 @@
+export const getPriority = (operation) => {
+  if (operation === '*'
+          || operation === '/'
+          || operation === '%'
+  ) {
+    return 2;
+  }
+
+  if (
+    operation === '+'
+          || operation === '-'
+  ) {
+    return 1;
+  }
+
+  return 0;
+};
