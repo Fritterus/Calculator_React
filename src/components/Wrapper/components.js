8a292c5487818ad1b4c402c285e72444ece71913
Fritterus <62291430+Fritterus@users.noreply.github.com>
2022-08-03 03:26:35 +0300

    Update components.js

--- src/components/Wrapper/components.js
+++ src/components/Wrapper/components.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
         width: 75%;
-        height: 90%;
+        height: 100%;
         padding: 10px;
         display:block;
 `;
